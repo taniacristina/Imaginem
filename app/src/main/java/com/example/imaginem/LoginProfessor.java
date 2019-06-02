c47a188a5a4f268cf287d9a6b0c4b1ff5743d5f3
Tânia Cristina <taniacristinasantosrocha@gmail.com>
2019-06-02 18:22:10 -0300

    Adicionando Atividade de Cadastro do Professor

--- app/src/main/java/com/example/imaginem/LoginProfessor.java
+++ app/src/main/java/com/example/imaginem/LoginProfessor.java
@@ -1,7 +1,9 @@
 package com.example.imaginem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginProfessor extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginProfessor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_professor);
+    }
+
+    public void cadastroProfessor(View view) {
+        Intent intentCadastroProf = new Intent(this, CadastroProfessor.class);
+        startActivity(intentCadastroProf);
     }
 }
